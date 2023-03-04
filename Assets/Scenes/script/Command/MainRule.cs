b538ec16b5dddee71fa9e5b0c9dde8a874774377
tory40 <zn072358@gmail.com>
2023-03-04 17:56:45 +0900

    コードの追加

--- Assets/Scenes/script/Command/MainRule.cs
+++ Assets/Scenes/script/Command/MainRule.cs
@@ -8,15 +8,16 @@ public class MainRule : MonoBehaviour
     public List<CommandObject> CommandList = new List<CommandObject>();
     public List<CommandObject> DefaultCommand = new List<CommandObject>();
     public CommandObject serectobject;
-    public int startpoint;
-    public int betpoint;
-    public float winmine;
-    public float losemine;
-    public float winenemy;
-    public float loseenemy;
-    public bool winbool;
-    public int winpoint;
-    public int endbattle;
+    public int startpoint=10000;
+    public int betpoint=100;
+    public float winmine=1;
+    public float losemine=0;
+    public float winenemy=1;
+    public float loseenemy=0;
+    public bool winbool=true;
+    public int winscore=999999;
+    public int endbattle=99;
+    public List<PokerType> types = new List<PokerType>();
 
     // Start is called before the first frame update
     void Start()
@@ -26,6 +27,13 @@ public class MainRule : MonoBehaviour
             CommandObject obj = new CommandObject();
             DefaultCommand.Add(obj);
         }
+        for (int i=0; i<27;++i)
+        {
+            PokerType typenum = new PokerType();
+            typenum.type = typenum.texts[i];
+            typenum.strong = i;
+            types.Add(typenum);
+        }
     }
 
     // Update is called once per frame
@@ -33,13 +41,18 @@ public class MainRule : MonoBehaviour
     {
         
     }
+    bool startget =true;
     public void CommandGet()
     {
-         for (int i = 1; i <= 30; ++i)
-         {
-             CommandObject obj = GameObject.Find("Toggle ("+i.ToString()+")").GetComponent<CommandObject>();
-             CommandList.Add(obj);
-         }
+        if (startget)
+        {
+            startget = false;
+            for (int i = 1; i <= 30; ++i)
+            {
+                CommandObject obj = GameObject.Find("Toggle (" + i.ToString() + ")").GetComponent<CommandObject>();
+                CommandList.Add(obj);
+            }
+        }
     }
     public void ChangeMine(int element)
     {
@@ -168,5 +181,143 @@ public class MainRule : MonoBehaviour
                 return;
         }
         CommandList[serectobject.objectnumber].Display(elementnum);
+    }
+    [SerializeField] InputField startinput;
+    [SerializeField] InputField betinput;
+    [SerializeField] InputField winmineinput;
+    [SerializeField] InputField winenemyinput;
+    [SerializeField] InputField losemineinput;
+    [SerializeField] InputField loseenemyinput;
+    [SerializeField] InputField winscoreinput;
+    [SerializeField] InputField endbattleinput;
+    [SerializeField] Text wintext;
+    [SerializeField] Text losetext;
+    [SerializeField] InputField[] inputstrong;
+    public void ChangeStart()
+    {
+        try
+        {
+            startpoint = (int)(float.Parse(startinput.text));
+            startinput.text = startpoint.ToString();
+        }
+        catch
+        {
+            Debug.Log("”¼Šp”š‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢");
+            startinput.text = startpoint.ToString();
+        }
+    }
+    public void ChangeBet()
+    {
+        try
+        {
+            betpoint = (int)(float.Parse(betinput.text));
+            betinput.text = betpoint.ToString();
+        }
+        catch
+        {
+            Debug.Log("”¼Šp”š‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢");
+            betinput.text = betpoint.ToString();
+        }
+    }
+    public void ChangeWinmine()
+    {
+        try
+        {
+            winmine = (float.Parse(winmineinput.text));
+        }
+        catch
+        {
+            Debug.Log("”¼Šp”š‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢");
+            winmineinput.text = winmine.ToString();
+        }
+    }
+    public void ChangeWinenemy()
+    {
+        try
+        {
+            winenemy = (float.Parse(winenemyinput.text));
+        }
+        catch
+        {
+            Debug.Log("”¼Šp”š‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢");
+            winenemyinput.text = winenemy.ToString();
+        }
+    }
+    public void ChangeLosemine()
+    {
+        try
+        {
+            losemine = (float.Parse(losemineinput.text));
+        }
+        catch
+        {
+            Debug.Log("”¼Šp”š‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢");
+            losemineinput.text = losemine.ToString();
+        }
+    }
+    public void ChangeLoseenemy()
+    {
+        try
+        {
+            loseenemy = (float.Parse(loseenemyinput.text));
+        }
+        catch
+        {
+            Debug.Log("”¼Šp”š‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢");
+            loseenemyinput.text = loseenemy.ToString();
+        }
+    }
+    public void ChangeWinscore()
+    {
+        try
+        {
+            winscore = (int)(float.Parse(winscoreinput.text));
+            winscoreinput.text = winscore.ToString();
+        }
+        catch
+        {
+            Debug.Log("”¼Šp”š‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢");
+            winscoreinput.text = winscore.ToString();
+        }
+    }
+    public void ChangeEndBattle()
+    {
+        try
+        {
+            endbattle = (int)(float.Parse(endbattleinput.text));
+            endbattleinput.text = endbattle.ToString();
+        }
+        catch
+        {
+            Debug.Log("”¼Šp”š‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢");
+            endbattleinput.text = endbattle.ToString();
+        }
+    }
+    public void ChangeWinbool()
+    {
+        winbool = !winbool;
+        if(winbool)
+        {
+            wintext.text = "Ÿ—˜ğŒ";
+            losetext.text = "”s–kğŒ";
+        }
+        else
+        {
+            wintext.text = "”s–kğŒ";
+            losetext.text = "Ÿ—˜ğŒ";
+        }
+    }
+    public void ChangeStrong(int input)
+    {
+        try
+        {
+            types[input].strong = float.Parse(inputstrong[input].text);
+            inputstrong[input].text = types[input].strong.ToString();
+        }
+        catch
+        {
+            Debug.Log("”¼Šp”š‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢");
+            inputstrong[input].text = types[input].strong.ToString();
+        }
     }
 }
---
+++ Assets/Scenes/script/Command/PokerType.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PokerType : MonoBehaviour
+{
+    public List<string> texts = new List<string> { "High card" , "One pair" , "One pair mark" , "Two pair" , "Three of a kind" , "High card flush" , "One pair flush" , "Straight" , "Two pair twin" , "Two pair mark",
+        "Three of a kind mark" ,"Full house","Full house duo","Full house torio","Two pair flush","Three of a kind flush","Four of a kind","Four of a kind half","Four of a kind rainbow","Five of a kind",
+    "Straight flush","Full house mark","Full house flush","Four of a kind mark","Four of a kind flush","Five of a kind half","Five of a kind flush"};
+    public float strong;
+    public string type;
+}
